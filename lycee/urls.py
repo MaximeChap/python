db08608e67d7adf1bcc1510c692b38ca9f258c74
Maxime Chapelle <maxime.chapelle@limayrac.fr>
2020-08-17 11:52:44 +0000

    creation pages

--- lycee/urls.py
+++ lycee/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     # /lycee/student/73
     url(r'^student/(?P<student_id>[0-9]+)$', views.detail_student, name='detail_student'),
     url(r'^student/create/$', StudentCreateView.as_view(), name='create_student'),
+
+
+
+    
 ]
